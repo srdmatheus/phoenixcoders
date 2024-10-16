76bb3445bb93d4ad104f0a1ea1aa895fc09fe4bb
Matheus Ribeiro <srdmatheus@gmail.com>
2024-10-16 17:48:57 -0300

    chore: change styles

--- src/components/Radio/RadioItem.tsx
+++ src/components/Radio/RadioItem.tsx
@@ -4,23 +4,31 @@ import { Indicator, Item } from "@radix-ui/react-radio-group";
 
 import { cn } from "@/lib/utils";
 
-export const RadioItem = forwardRef<
-  ElementRef<typeof Item>,
-  ComponentPropsWithoutRef<typeof Item>
->(({ className, ...props }, ref) => {
-  return (
-    <Item
-      ref={ref}
-      className={cn(
-        "aspect-square size-4 rounded-full border border-black",
-        className
-      )}
-      {...props}
-    >
-      <Indicator className="flex items-center justify-center">
-        <div className="size-2.5 rounded-full bg-black" />
-      </Indicator>
-    </Item>
-  );
-});
+type RadioItemProps = ComponentPropsWithoutRef<typeof Item> & {
+  name: string;
+};
+
+export const RadioItem = forwardRef<ElementRef<typeof Item>, RadioItemProps>(
+  ({ className, name, ...props }, ref) => {
+    return (
+      <label className="flex items-center space-x-2 text-gray-2">
+        <Item
+          ref={ref}
+          className={cn(
+            "aspect-square size-5 rounded-full border-2 border-gray-2",
+            "data-[state=checked]:border-blue-1",
+            className
+          )}
+          {...props}
+        >
+          <Indicator className="flex items-center justify-center">
+            <div className="size-3 rounded-full bg-blue-1" />
+          </Indicator>
+        </Item>
+        <span className="cursor-pointer">{name}</span>
+      </label>
+    );
+  }
+);
+
 RadioItem.displayName = Item.displayName;
